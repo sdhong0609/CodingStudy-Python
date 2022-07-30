8641558fda9ca5985b1045d41aaae9f214c5ffae
hong <hsd1525@naver.com>
2022-07-30 14:33:54 +0900

    practiced string and functions

--- main.py
+++ main.py
@@ -1,9 +1,45 @@
-# for i in range(5):
-#     for j in range(i+1):
-#         print("*", end=" ")
-#     print()
+# msg = "It is Time"
+# print(msg.upper())
+# print(msg.lower())
+# print(msg)
+# tmp = msg.upper()
+# print(tmp)
+# print(tmp.find("T"))
+# print(tmp.count('T'))
+# print(msg)
+# print(msg[:2])
+# print(msg[3:5])
+# print(len(msg))
+# for i in range(len(msg)):
+#     print(msg[i], end=' ')
+# print()
+#
+# for x in msg:
+#     print(x, end=' ')
+# print()
+#
+# for x in msg:
+#     if x.isupper():
+#         print(x, end=' ')
+# print()
+#
+# for x in msg:
+#     if x.islower():
+#         print(x, end=' ')
+# print()
+#
+# for x in msg:
+#     if x.isalpha():
+#         print(x, end='')
+# print()
 
-for i in range(5):
-    for j in range(5-i):
-        print("*", end=" ")
-    print()
+tmp = "AZ"
+for x in tmp:
+    print(ord(x))
+
+tmp = "az"
+for x in tmp:
+    print(ord(x))
+
+tmp = 66
+print(chr(tmp))
