14e4d29d3d655996ef0f683f8fe616cc67250633
hong <sdhong0609@gmail.com>
2022-07-31 19:43:28 +0900

    practice two-dimensional list

--- main.py
+++ main.py
@@ -1,41 +1,17 @@
-# a = [23, 12, 36, 53, 19]
-# print(a[:3])
-# print(a[1:4])
-# print(len(a))
-#
-# for i in range(len(a)):
-#     print(a[i], end=' ')
-# print()
-#
-# for x in a:
-#     if x % 2 == 1:
-#         print(x, end=' ')
-# print()
+# a = [0] * 3
+# print(a)
 
-# a = [23, 12, 36, 53, 19]
-# for x in enumerate(a):
-#     print(x)
+a = [[0] * 3 for _ in range(3)]
+# print(a)
+a[0][1] = 1
+# print(a)
+a[1][1] = 2
+# print(a)
 
-# b = (1, 2, 3, 4, 5)
-# print(b[0])
-# b[0] = 7
+for x in a:
+    print(x)
 
-# for x in enumerate(a):
-#     print(x[0], x[1])
-# print()
-
-a = [23, 12, 36, 53, 19]
-
-for index, value in enumerate(a):
-    print(index, value)
-print()
-
-if all(x < 60 for x in a):
-    print("YES")
-else:
-    print("NO")
-
-if any(x < 11 for x in a):
-    print("YES")
-else:
-    print("NO")
+for x in a:
+    for y in x:
+        print(y, end=' ')
+    print()
