b91a3988fbb9439afcff5f09a5062b995d5be5be
hong <sdhong0609@gmail.com>
2022-08-17 21:17:33 +0900

    Solve Question 8 Second

--- Section2/Q_08.py
+++ Section2/Q_08.py
@@ -1,38 +1,29 @@
 # 뒤집은 소수
 
 # 내 풀이
-# n = int(input())
-# arr = list(map(int, input().split()))
-#
-#
-# def reverse(x):
-#     cnt = len(str(x)) - 1
-#     answer = 0
-#     while x > 0:
-#         answer += x % 10 * (10 ** cnt)
-#         x = x // 10
-#         cnt -= 1
-#     return answer
-#
-#
-# def isPrime(x):
-#     if x == 1:
-#         return False
-#     isPrime = True
-#     y = int(x ** (1 / 2))
-#     for i in range(2, y + 1):
-#         if x % i == 0:
-#             isPrime = False
-#             break
-#     return isPrime
-#
-#
-# for i in range(len(arr)):
-#     arr[i] = reverse(arr[i])
-#
-# for i, x in enumerate(arr):
-#     if isPrime(x):
-#         print(x, end=' ')
+n = int(input())
+arr = list(map(int, input().split()))
+
+def reverse(x):
+    res = 0
+    while x > 0:
+        res = res * 10 + x % 10
+        x = x // 10
+    return res
+
+def isPrime(x):
+    if x == 1:
+        return False
+    for i in range(2, x):
+        if x % i == 0:
+            return False
+    else:
+        return True
+
+for x in arr:
+    tmp = reverse(x)
+    if isPrime(tmp):
+        print(tmp, end=' ')
 
 
 # 강사 풀이
