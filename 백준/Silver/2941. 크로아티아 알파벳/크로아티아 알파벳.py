9ea86634abdb146862ee9a197708ca6c0701350f
Seongdeok Hong <sdhong0609@gmail.com>
2023-03-16 11:57:30 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -1,10 +1,8 @@
 arr = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
-cnt = 0
 s = input()
+
 for x in arr:
     if x in s:
-        cnt += s.count(x)
-        s = s.replace(x, ' ')
+        s = s.replace(x, 'a')
 
-s = s.replace(' ', '')
-print(cnt + len(s))
+print(len(s))
