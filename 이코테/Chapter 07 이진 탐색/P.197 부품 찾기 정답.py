3f5ef2e2a0133b028c70b94ad6586198aea88fec
hong <sdhong0609@gmail.com>
2023-04-01 01:46:54 +0900

    Chapter 07 이진 탐색 문제 정답

---
+++ 이코테/Chapter 07 이진 탐색/P.197 부품 찾기 정답.py
@@ -0,0 +1,82 @@
+""" P.197 부품 찾기 정답 """
+
+""" 책 풀이 1 (이진 탐색 이용) """
+# 이진 탐색 소스코드 구현(반복문)
+def binary_search(array, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return None
+
+
+# N(가게의 부품 개수) 입력
+n = int(input())
+# 가게에 있는 전체 부품 번호를 공백으로 구분하여 입력
+array = list(map(int, input().split()))
+array.sort()  # 이진 탐색을 수행하기 위해 사전에 정렬 수행
+# M(손님이 확인 요청한 부품 개수) 입력
+m = int(input())
+# 손님이 확인 요청한 전체 부품 번호를 공백으로 구분하여 입력
+x = list(map(int, input().split()))
+
+# 손님이 확인 요청한 부품 번호를 하나씩 확인
+for i in x:
+    # 해당 부품이 존재하는지 확인
+    result = binary_search(array, i, 0, n - 1)
+    if result is not None:
+        print('yes', end=' ')
+    else:
+        print('no', end=' ')
+
+
+""" 책 풀이 2 (계수 정렬 이용) """
+# N(가게의 부품 개수)을 입력받기
+n = int(input())
+array = [0] * 1000001
+
+# 가게에 있는 전체 부품 번호를 입력받아서 기록
+for i in input().split():
+    array[int(i)] = 1
+
+# M(손님이 확인 요청한 부품 개수)을 입력받기
+m = int(input())
+# 손님이 확인 요청한 전체 부품 번호를 공백으로 구분하여 입력
+x = list(map(int, input().split()))
+
+# 손님이 확인 요청한 부품 번호를 하나씩 확인
+for i in x:
+    # 해당 부품이 존재하는지 확인
+    if array[i] == 1:
+        print('yes', end=' ')
+    else:
+        print('no', end=' ')
+
+
+""" 책 풀이 3 (집합 자료형 이용) """
+"""
+집합 자료형은 단순히 특정한 데이터가 존재하는지
+검사할 때에 매우 효과적으로 사용할 수 있다.
+set() 함수는 집합 자료형을 초기화할 때 사용한다.
+"""
+# N(가게의 부품 개수)을 입력받기
+n = int(input())
+# 가게에 있는 전체 부품 번호를 입력받아서 집합(set) 자료형에 기록
+array = set(map(int, input().split()))
+
+# M(손님이 확인 요청한 부품 개수)을 입력받기
+m = int(input())
+# 손님이 확인 요청한 전체 부품 번호를 공백으로 구분하여 입력
+x = list(map(int, input().split()))
+
+# 손님이 확인 요청한 부품 번호를 하나씩 확인
+for i in x:
+    # 해당 부품이 존재하는지 확인
+    if i in array:
+        print('yes', end=' ')
+    else:
+        print('no', end=' ')
