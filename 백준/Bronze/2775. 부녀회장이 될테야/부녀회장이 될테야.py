0cb7b4a6fb8b4505914ffbec9d326d913dc209a8
Seongdeok Hong <sdhong0609@gmail.com>
2023-04-04 17:47:24 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 64 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -0,0 +1,15 @@
+t = int(input())
+
+for _ in range(t):
+    k = int(input())
+    n = int(input())
+    d = [[0] * (n + 1) for _ in range(k + 1)]
+    for i in range(1, n + 1):
+        d[0][i] = i
+
+    for i in range(1, k + 1):
+        for j in range(1, n + 1):
+            for x in range(1, j + 1):
+                d[i][j] += d[i - 1][x]
+
+    print(d[k][n])
