636738c4e88ed8b640d70acac09937a2dbd136e5
hong <sdhong0609@gmail.com>
2023-04-10 22:32:49 +0900

    Chapter 09 최단 경로 문제 정답

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
-arr = [5, 3, 7, 9, 2, 5, 2, 6]
+""" P.262 전보 """
 
-# arrMin = float('inf')  # python 에서 가장 큰 값 (무한대)
-arrMin = arr[0]
-for i in range(len(arr)):
-    if arr[i] < arrMin:
-        arrMin = arr[i]
-print(arrMin)
+""" 내 풀이 """
+import heapq
+import sys
 
-arrMin = float('inf')
-for x in arr:
-    if x < arrMin:
-        arrMin = x
-print(arrMin)
+input = sys.stdin.readline
+INF = int(1e9)
+n, m, c = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+distance = [INF] * (n + 1)  #
+
+for _ in range(m):
+    x, y, z = map(int, input().split())
+    graph[x].append((y, z))  # (목적지 노드, 거리) 순
+
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:
+        dist, now = heapq.heappop(q)  # (거리, 노드) 순
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+
+dijkstra(c)
+
+distance.remove(0)
+distance.remove(INF)
+print(len(distance), max(distance))
