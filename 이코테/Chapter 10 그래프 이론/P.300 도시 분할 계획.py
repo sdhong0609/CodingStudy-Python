5f795fe5a26cb2386af1366f30f69fabbe79362b
hong <sdhong0609@gmail.com>
2023-04-17 19:18:36 +0900

    Chapter 10 그래프 P.300 도시 분할 계획 문제풀이 및 정답

---
+++ 이코테/Chapter 10 그래프 이론/P.300 도시 분할 계획.py
@@ -0,0 +1,63 @@
+""" P.300 도시 분할 계획 """
+
+""" 내 풀이 """
+import sys
+
+input = sys.stdin.readline
+
+
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+n, m = map(int, input().split())
+parent = [0] * (n + 1)
+
+edges = []
+result = []
+
+for i in range(1, n + 1):
+    parent[i] = i
+
+for _ in range(m):
+    a, b, cost = map(int, input().split())
+    edges.append((cost, a, b))
+
+edges.sort()
+
+for edge in edges:
+    cost, a, b = edge
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result.append(cost)
+
+print(sum(result) - max(result))
+
+# 입력
+# 7 12
+# 1 2 3
+# 1 3 2
+# 3 2 1
+# 2 5 2
+# 3 4 4
+# 7 3 6
+# 5 1 5
+# 1 6 2
+# 6 4 1
+# 6 5 3
+# 4 5 3
+# 6 7 4
+
+# 출력
+# 8
