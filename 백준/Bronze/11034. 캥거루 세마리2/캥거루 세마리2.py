331f697bdc01ba6f5ed4a642996a241f27f4ba85
Seongdeok Hong <sdhong0609@gmail.com>
2023-05-05 19:58:56 +0900

    [Bronze III] Title: 캥거루 세마리2, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/11034. 캥거루 세마리2/캥거루 세마리2.py
+++ 백준/Bronze/11034. 캥거루 세마리2/캥거루 세마리2.py
@@ -1,19 +1,28 @@
+# """ 풀이 1"""
+# while True:
+#     try:
+#         a, b, c = map(int, input().split())
+#         count = 0
+#         while True:
+#             if a + 2 == b + 1 == c:
+#                 break
+# 
+#             if c - b > b - a:
+#                 a, b, c = b, b + 1, c
+#                 count += 1
+#             else:
+#                 a, b, c = a, b - 1, b
+#                 count += 1
+# 
+#         print(count)
+#     except EOFError:
+#         break
+
+""" 풀이 2"""
 while True:
     try:
         a, b, c = map(int, input().split())
-        count = 0
-        while True:
-            if a + 2 == b + 1 == c:
-                break
-
-            if c - b > b - a:
-                a, b, c = b, b + 1, c
-                count += 1
-            else:
-                a, b, c = a, b - 1, b
-                count += 1
-
-        print(count)
+        print(max(b - a, c - b) - 1)
     except EOFError:
         break
 
