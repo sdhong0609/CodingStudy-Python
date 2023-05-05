15ae09219930739b42afd29d4c9d375338428e5a
Seongdeok Hong <sdhong0609@gmail.com>
2023-05-05 21:22:02 +0900

    [Bronze II] Title: 욱제는 효도쟁이야!!, Time: 56 ms, Memory: 36220 KB -BaekjoonHub

--- 백준/Bronze/14487. 욱제는 효도쟁이야！！/욱제는 효도쟁이야！！.py
+++ 백준/Bronze/14487. 욱제는 효도쟁이야！！/욱제는 효도쟁이야！！.py
@@ -2,15 +2,3 @@ n = int(input())
 costs = list(map(int, input().split()))
 
 print(sum(costs) - max(costs))
-
-# 입력 1
-# 5
-# 1 6 5 2 4
-# 출력 1
-# 12
-
-# 입력 2
-# 4
-# 100 100 100 101
-# 출력 2
-# 300
