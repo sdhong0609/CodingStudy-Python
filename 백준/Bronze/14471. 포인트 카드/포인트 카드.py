a342bca61875727a3b2a1fab400703322dd813e9
Seongdeok Hong <sdhong0609@gmail.com>
2023-05-05 23:14:47 +0900

    [Bronze II] Title: 포인트 카드, Time: 104 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/14471. 포인트 카드/포인트 카드.py
@@ -0,0 +1,23 @@
+n, m = map(int, input().split())
+present = 0
+target = m - 1
+cards = []
+for _ in range(m):
+    win, lose = map(int, input().split())
+    if win >= n:
+        present += 1
+    else:
+        cards.append((win, lose))
+
+result = 0
+diff = []
+if present >= target:
+    print(0)
+else:
+    target -= present
+    for card in cards:
+        diff.append(n - card[0])
+    diff.sort()
+    for i in range(target):
+        result += diff[i]
+    print(result)
