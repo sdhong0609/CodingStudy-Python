bcc7269eea9c0ec374f37b246696a5aa65d915c3
Seongdeok Hong <sdhong0609@gmail.com>
2023-05-11 17:48:37 +0900

    [Bronze I] Title: 제자리, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/25400. 제자리/제자리.py
@@ -0,0 +1,16 @@
+n = int(input())
+array = list(map(int, input().split()))
+
+i = 0
+count = 0
+while True:
+    if i >= n:
+        break
+    if array[i] != i+1:
+        array.pop(i)
+        count += 1
+        n -= 1
+        i -= 1
+    i += 1
+
+print(count)
